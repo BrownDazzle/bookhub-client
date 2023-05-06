bf3b211da2a33cbaa17fd6d7b575bc6a1854fbf0
BrownDazzle <actscloud28@gmail.com>
2023-05-06 12:14:41 +0200

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  pages: {
+    index: {
+      entry: 'src/main.jsx',
+      template: 'index.html',
+      filename: 'index.html',
+    },
+    home: {
+      entry: 'src/pages/Home.jsx',
+      template: 'index.html',
+      filename: 'home.html',
+    },
+  },
 })
